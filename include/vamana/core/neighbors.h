b28193b8fde61ab120c4ffdfff914d2a703f1092
abg0148 <abhinavgupta0148@gmail.com>
2025-10-01 19:51:58 -0400

    added neighbor header and defined methods for sorting, dedupplicating neighbor list

---
+++ include/vamana/core/neighbors.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "types.h"
+#include <queue>
+#include <vector>
+
+struct Neighbor{
+    location_t id;
+    distance_t distance;
+
+    // constructor
+    Neighbor(location_t id_, distance_t distance_): id(id_), distance(distance_){}
+
+    // operator overloads for priority queue
+    bool operator<(const Neighbor& other) const{
+        return distance > other.distance;   // reverse order for min-heap, basically we want the smallest distance to have the highest priority
+    }
+
+    bool operator>(const Neighbor& other) const{
+        return distance < other.distance;   // smaller the distance, higher the priority in the min heap based priority queue
+    }
+
+    bool operator==(const Neighbor& other) const{
+        return id == other.id;               // equality based on id only. we could also use distance too. basically if we want to avoid duplicates in our neighbor lists
+    }
+
+};
+
+typedef std::priority_queue<Neighbor> NeighborPriorityQueue; // min-heap based priority queue for neighbors
+
---
+++ src/core/neighbor.cpp
@@ -0,0 +1,37 @@
+#include "neighbors.h"
+#include <algorithm>
+
+void sort_neighbors_by_distance(std::vector<Neighbor>& neighbors){
+    std::sort(
+        neighbors.begin(), 
+        neighbors.end(), 
+        [](const Neighbor& a, const Neighbor& b){
+            return a.distance < b.distance;
+        });
+};
+
+void remove_duplicate_neighbors(std::vector<Neighbor>& neighbors){
+    // sort th neighbors by id
+    // this takes O(n log n)
+    std::sort(
+        neighbors.begin(),
+        neighbors.end(),
+        [](const Neighbor& a, const Neighbor&b){
+            return a.id < b.id;
+        }
+    );
+
+    // use std::unique to get iterator to new end of unique range
+    // this takes O(n)
+    auto last = std::unique(
+        neighbors.begin(),
+        neighbors.end(),
+        [](const Neighbor& a, const Neighbor&b){
+            return a.id == b.id;
+        }
+    );
+
+    // erase the non-unique elements
+    // this takes O(1)
+    neighbors.erase(last, neighbors.end());
+}

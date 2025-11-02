1444c1867e9f65146f3496e2a96650460b2f650c
gptabhinav <ag10706@nyu.edu>
2025-11-02 18:46:16 -0500

    added a configuration to allow easy downloads and conversion of datasets

---
+++ config.mk
@@ -0,0 +1,14 @@
+# this seems to work well for sift and siftsmall
+dataset = siftsmall
+
+dataset_root =  datasets/${dataset}
+
+dataset.base = ${dataset_root}/$(dataset)_base.fvecs
+dataset.query = ${dataset_root}/$(dataset)_query.fvecs
+dataset.groundtruth = ${dataset_root}/$(dataset)_groundtruth.ivecs
+
+dataset_bin_root = ${dataset_root}/bin
+
+dataset.base.bin = ${dataset_bin_root}/$(dataset)_base.fbin
+dataset.query.bin = ${dataset_bin_root}/$(dataset)_query.fbin
+dataset.groundtruth.bin = ${dataset_bin_root}/$(dataset)_groundtruth.ibin

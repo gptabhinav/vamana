77b9a8b838da7b8d885fef788bf0585be9976a3a
gptabhinav <ag10706@nyu.edu>
2025-11-03 14:29:22 -0500

    added output to gitignore, added configuration for index location and results output

--- config.mk
+++ config.mk
@@ -1,7 +1,13 @@
 # use this for runtime configuration
 # for static constants in the code, consider looking at types.h
 
-# this seems to work well for sift and siftsmall
+#graph construction parameters
+R = 32
+L = 64
+alpha = 1.2
+
+# this configuration format seems to work well for sift and siftsmall
+# might have to update this when i work with glove, gist and others
 dataset = siftsmall
 
 dataset_root =  datasets/${dataset}
@@ -15,3 +21,11 @@ dataset_bin_root = ${dataset_root}/bin
 dataset.base.bin = ${dataset_bin_root}/$(dataset)_base.fbin
 dataset.query.bin = ${dataset_bin_root}/$(dataset)_query.fbin
 dataset.groundtruth.bin = ${dataset_bin_root}/$(dataset)_groundtruth.ibin
+
+index_root = index/
+
+dataset.index = ${index_root}/$(dataset)
+
+results_root = output/
+
+dataset.results = ${results_root}/${dataset}.csv
